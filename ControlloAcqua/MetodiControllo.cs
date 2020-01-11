232251c946acaab017ade87109dc9c82ab126d46
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-11 12:20:27 +0100

    fatto il metodo temperatura

--- ControlloAcqua/MetodiControllo.cs
+++ ControlloAcqua/MetodiControllo.cs
@@ -4,5 +4,21 @@ namespace ControlloAcqua
 {
     public class MetodiControllo
     {
+        public static bool ControlloTemperatura(double temperatura)
+        {
+            int tempmin = 12;
+            int tempmax = 25;
+            bool risposta;
+            if(temperatura>=tempmin||temperatura<=tempmax)
+            {
+                risposta = true;
+                return risposta;
+            }
+            else
+            {
+                risposta = false;
+                return risposta;
+            }
+        }
     }
 }
