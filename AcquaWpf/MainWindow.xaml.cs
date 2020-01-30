6ec671bdccc3888b78b31211d43b58114a147fb1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-30 12:53:48 +0100

    manca la parte grafica

--- AcquaWpf/MainWindow.xaml.cs
+++ AcquaWpf/MainWindow.xaml.cs
@@ -42,24 +42,24 @@ namespace AcquaWpf
                 bool rispT = Metodi.ControlloTemperatura(t);
                 if (rispT == true)
                 {
-                    lblresult.Content += "Temperatura:si \n";
+                    lblresult.Content += "Temperatura : si \n";
                      f1 = true;
                 }
                 else
                 {
-                    lblresult.Content += "Temperatura:no \n";
+                    lblresult.Content += "Temperatura : no \n";
                      f1 = false;
                 }
                 double p = double.Parse(ph.Text);
                 bool rispP = Metodi.ControlloPh(p);
                 if (rispP == true)
                 {
-                    lblresult.Content += "Ph:si \n";
+                    lblresult.Content += "Ph : si \n";
                      f2 = true;
                 }
                 else
                 {
-                    lblresult.Content += "Ph:no \n";
+                    lblresult.Content += "Ph : no \n";
                     f2 = false;
                 }
                 double r = double.Parse(residuo.Text);
@@ -67,12 +67,12 @@ namespace AcquaWpf
                 bool rispR = Metodi.ControlloResiduo(r, l1);
                 if (rispR == true)
                 {
-                    lblresult.Content += "Residuo:si \n";
+                    lblresult.Content += "Residuo : si \n";
                      f3 = true;
                 }
                 else
                 {
-                    lblresult.Content += "Residuo:no \n";
+                    lblresult.Content += "Residuo : no  \n";
                     f3 = false;
 
                 }
@@ -81,24 +81,24 @@ namespace AcquaWpf
                 bool rispA = Metodi.ControlloAmmoniaca(a, l2);
                 if (rispA == true)
                 {
-                    lblresult.Content += "Ammoniaca:si \n";
+                    lblresult.Content += "Ammoniaca : si \n";
                      f4 = true;
                 }
                 else
                 {
-                    lblresult.Content += "Ammoniaca:no \n";
+                    lblresult.Content += "Ammoniaca : no \n";
                      f4 = false;
                 }
                 double ni = double.Parse(nitriti.Text);
                 bool rispNi = Metodi.ControlloNitriti(ni);
                 if (rispNi == true)
                 {
-                    lblresult.Content += "Nitriti:si \n";
+                    lblresult.Content += "Nitriti : si \n";
                      f5 = true;
                 }
                 else
                 {
-                    lblresult.Content += "Nitriti:no \n";
+                    lblresult.Content += "Nitriti : no \n";
                      f5 = false;
                 }
                 double na = double.Parse(nitrati.Text);
@@ -106,12 +106,12 @@ namespace AcquaWpf
                 bool rispNa = Metodi.ControlloNitrati(na, l3);
                 if (rispNa == true)
                 {
-                    lblresult.Content += "Nitrati:si \n";
+                    lblresult.Content += "Nitrati : si \n";
                      f6 = true;
                 }
                 else
                 {
-                    lblresult.Content += "Nitrati:no \n";
+                    lblresult.Content += "Nitrati : no \n";
                      f6 = false;
                 }
                 double c = double.Parse(cloruri.Text);
@@ -119,12 +119,12 @@ namespace AcquaWpf
                 bool rispC = Metodi.ControlloCloruri(a, l4);
                 if (rispC == true)
                 {
-                    lblresult.Content += "Cloruri:si \n";
+                    lblresult.Content += "Cloruri : si \n";
                      f7 = true;
                 }
                 else
                 {
-                    lblresult.Content += "Cloruri:no \n";
+                    lblresult.Content += "Cloruri : no \n";
                      f7 = false;
                 }
             }
@@ -134,14 +134,14 @@ namespace AcquaWpf
             }
             finally
             {
-                ph.Text = "inserisci il ph";
-                cloruri.Text = "inserisci i cloruri(mg/l)";
-                nitrati.Text = "inserisci i nitrati(mg/l)";
-                nitriti.Text = "inserisci i nitriti(mg/l)";
-                litri.Text = "inserisci i litri";
-                ammoniaca.Text = "inserisci l'ammoniaca(mg/l)";
-                residuo.Text = "inserisci il residuo(mg/l)";
-                temperatura.Text = "inserisci la temperatura(C°)";
+                ph.Text = " ph";
+                cloruri.Text = "cloruri(mg/l)";
+                nitrati.Text = "nitrati(mg/l)";
+                nitriti.Text = "nitriti(mg/l)";
+                litri.Text = "litri";
+                ammoniaca.Text = "ammoniaca(mg/l)";
+                residuo.Text = "residuo(mg/l)";
+                temperatura.Text = "temperatura(C°)";
             }
             if(f1&&f2&&f3&&f4&&f5&&f6&&f7==true)
             {
@@ -155,6 +155,48 @@ namespace AcquaWpf
                 lblresult.Content += (" \n");
                 lblresult.Content += ("L'acqua non è potabile");
             }
+        }
+
+        private void Cloruri_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            cloruri.Clear();
+        }
+
+        
+
+        private void Nitrati_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            nitrati.Clear();
+        }
+
+        private void Litri_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            litri.Clear();
+        }
+
+        private void Nitriti_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            nitriti.Clear();
+        }
+
+        private void Residuo_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            residuo.Clear();
+        }
+
+        private void Temperatura_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            temperatura.Clear();
+        }
+
+        private void Ammoniaca_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            ammoniaca.Clear();
+        }
+
+        private void Ph_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            ph.Clear();
         }
     }
 }
